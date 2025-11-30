f27c9bb4dddb205dbd725630fd8e3c1c11ae38ee
+ <+>
2025-11-30 17:19:11 +0300

    Add Render URL to CORS

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ SIMPLE_JWT = {
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "https://fundigo25.netlify.app",
+    "https://fg-1-ltam.onrender.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
